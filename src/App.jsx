1a806f1ed0079f4bccc3818dc41547ce66eadc22
mohit <mohitsasane76@gmail.com>
2024-06-08 01:42:18 +0530

    adeed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import img1 from "./assets/sid.jpg"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="nptl">
+                <span><a href={img1} download="dbms certificate nptel.jpg">Course Certificate</a></span>
+      </div>
+    </>
+  )
+}
+
+export default App
